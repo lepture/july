bf320101a959b86a57e24dda7834b4af1c7fe712
lepture <lepture@me.com>
2012-04-12 15:36:52 +0800

    write setup file

---
+++ junetornado/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""June Tornado
+"""
+
+__version__ = '0.1'
+__author__ = 'Hsiaoming Young'
--- setup.py
+++ setup.py
@@ -1,20 +1,33 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+from setuptools import setup
+from junetornado import __version__
 
 setup(
     name='junetornado',
-    version='0.1',
+    version=__version__,
     author='Hsiaoming Young',
     author_email='lepture@me.com',
     url='http://lepture.com/project/june',
-    packages=find_packages(),
+    packages=['junetornado', 'junetornado.ext'],
     description='June Tornado: a better way to organize your tornado project',
+    scripts=['junetornado/bin/june.admin'],
     install_requires=[
         'tornado',
         'SQLAlchemy',
     ],
     include_package_data=True,
     license='BSD License',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
 )
