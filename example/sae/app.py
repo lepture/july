a57eb7b2ae209af81506bd82ad8f95819a16a6d9
lepture <lepture@me.com>
2012-04-29 10:56:27 +0800

    fix on sae example

--- example/sae/app.py
+++ example/sae/app.py
@@ -1,8 +1,13 @@
 import sae
 from tornado.options import options
+from sqlalchemy.pool import NullPool
 options.sqlalchemy_master = 'mysql://%s:%s@%s:%s/%s?charset=utf8' %\
         (sae.const.MYSQL_USER, sae.const.MYSQL_PASS, sae.const.MYSQL_HOST,
          sae.const.MYSQL_PORT, sae.const.MYSQL_DB)
+
+options.sqlalchemy_kwargs = {
+    'poolclass': NullPool,
+}
 
 from july import JulyHandler, JulyApplication
 from july.database import db
@@ -18,4 +23,4 @@ handlers = [
     ('/', HomeHandler),
 ]
 
-app = JulyApplication(handlers, wsgi=True)
+app = JulyApplication(handlers, wsgi=True, debug=True)
