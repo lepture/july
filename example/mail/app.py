bf9440eb1e1d923e42ac5cf4f49014605c8f9f0f
lepture <lepture@me.com>
2012-05-19 22:16:52 +0800

    mail example

---
+++ example/mail/app.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from tornado.options import define
+define('smtp_user', 'root@gmail.com')
+define('smtp_password', 'password')
+define('smtp_host', 'smtp.gmail.com')
+define('smtp_ssl', True)
+
+from july.web import JulyHandler, run_server
+from july.app import JulyApplication
+from july.ext import webservice
+from july.ext.mail import mail_app
+
+import os.path
+ROOT = os.path.abspath(os.path.dirname(__file__))
+
+
+class HomeHandler(JulyHandler):
+    def get(self):
+        self.render('home.html')
+
+    def post(self):
+        user = self.get_argument('user')
+        subject = self.get_argument('subject')
+        body = self.get_argument('body')
+        dct = dict(user=user, subject=subject, body=body, subtype='html')
+        self.redirect('/')
+        webservice.post('mail/outbox', dct)
+
+
+handlers = [
+    ('/', HomeHandler),
+]
+
+
+settings = dict(
+    template_path=os.path.join(ROOT, 'templates'),
+)
+
+application = JulyApplication(handlers=handlers, debug=True, **settings)
+
+application.register_app(mail_app, url_prefix='/mail')
+
+if __name__ == '__main__':
+    run_server(application)
