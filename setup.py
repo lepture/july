3369a286af74996e6e549ccc9a2db7ac1d2cf419
lepture <lepture@me.com>
2012-06-06 22:20:00 +0800

    july __doc__

--- setup.py
+++ setup.py
@@ -7,17 +7,18 @@ major, minor = sys.version_info[:2]
 if major >= 3:
     kwargs['use_2to3'] = True
 
+import july
 from setuptools import setup, find_packages
-from july import __version__, __author__
 
 setup(
     name='july',
-    version=__version__,
-    author=__author__,
+    version=july.__version__,
+    author=july.__author__,
     author_email='lepture@me.com',
     url='http://project.lepture.com/july/',
     packages=find_packages(),
     description='July Tornado: a better way to organize your tornado project',
+    long_description=july.__doc__,
     scripts=['scripts/july'],
     install_requires=[
         'tornado',
